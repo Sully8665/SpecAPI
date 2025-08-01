17e1c4d6285330f5d1044f8c3681d8830fcfd7da
Saleh <saleh.rezaei@gmail.com>
2025-08-01 22:58:04 +0400

    restructure

--- src/SpecAPI/Program.cs
+++ src/SpecAPI/Program.cs
@@ -5,8 +5,6 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        Console.WriteLine("Welcome to SpecAPI Free version!");
-
         if (args.Length != 1)
         {
             Console.WriteLine("Usage: dotnet run <path-to-yaml-file>");
